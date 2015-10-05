a4ed8ecde89c9c81ec05df3246e97b0c543cfd5b
Makoto OOSUGI <oosugi20@gmail.com>
2015-10-05 20:59:37 +0900

    テーブルのコード整理

--- modules/TableForMakeleaps.js
+++ modules/TableForMakeleaps.js
@@ -1,28 +1,42 @@
+// requires
 var _ = require('underscore');
 var fs = require('fs');
 
 
-
-
 /**
+ * Makeleaps用テーブルデータのコンストラクタ。
  * @constructor
  * @param {Estimate} model
+ * @see ../run.js
  */
 var Table = function (model) {
 
+	/**
+	 * 引数に渡されたモデルを保持する。
+	 * @type {Estimate}
+	 * @see #_create
+	 * @see #_createEstimateRow
+	 * @see #_createCommodityRow
+	 */
 	this.model = model;
 
-	this.rows = [];
-
-	this._create();
+	/**
+	 * Arrayでテーブルデータを保持する。
+	 * @type {array}
+	 * @see #getCSVString
+	 * @see #_create
+	 */
+	this.rows = this._create();
 
 };
 
 
-(function (fn) {
+(function (fn) { // prototype shortcut
 
-
-
+	/**
+	 * ヘッダー行
+	 * @private
+	 */
 	var headerRow = (function () {
 		var COL_SRC = '行形式	書類番号	発行日	クライアント名	クライアント住所形式	クライアント国名	クライアント郵便番号	クライアント県名	クライアント市区町村	クライアント番地	クライアントマンション・ビル名	クライアント電話番号	クライアントEメール(to)	クライアントEメール(cc)	クライアント担当者部門	クライアント担当者肩書	クライアント担当者	項目タイプ	商品コード	項目名	項目単価	項目数量	項目単位	項目金額	項目消費税対象	項目消費税込み	項目源泉税対象	小計	消費税率	消費税	源泉税	合計金額	通貨	支払日	送信日	振込先タイトル	振込先	件名	備考	メモ	タグ	テンプレート	支払期限	有効期限	納品日	但し';
 		var cols = COL_SRC.split(/\t/);
@@ -40,6 +54,12 @@ var Table = function (model) {
 	})();
 
 
+	/**
+	 * 同期処理でデータからCSV形式のファイルで書き出す。
+	 * @param {string} write_file_name
+	 * @requires #getCSVString
+	 * @see ../run.js
+	 */
 	fn.writeFileSync = function (write_file_name) {
 
 		var string = this.getCSVString();
@@ -50,6 +70,8 @@ var Table = function (model) {
 
 
 	/**
+	 * Array形式のテーブルデータをCSV形式のStringに変換して返す。
+	 * @see #writeFileSync
 	 * @todo エスケープ処理
 	 **/
 	fn.getCSVString = function () {
@@ -65,49 +87,101 @@ var Table = function (model) {
 	};
 
 
+	/**
+	 * 行データを作成して返す。
+	 * @return {array} 行データ
+	 * @requires headerRow.cols
+	 * @requires #_createEstimateRow
+	 * @requires .model.commodities.items
+	 * @requires #_createCommodityRow
+	 * @see CommodityCollection
+	 * @see Commodity
+	 */
 	fn._create = function () {
 
-		this.rows[0] = headerRow.cols;
+		var rows = [];
 
-		// TODO ここに見積書の行
-		this.rows.push(this._createEstimateRow());
+		// ヘッダー行を追加
+		rows[0] = headerRow.cols;
 
+		// 見積書用の最初の行
+		rows[1] = this._createEstimateRow();
+
+		// 各見積書項目行
 		_.each(this.model.commodities.items, function (commodity) {
-			this.rows.push(this._createCommodityRow(commodity));
+			rows.push(this._createCommodityRow(commodity));
 		}.bind(this));
+
+		return rows;
 
 	};
 
 
 	/**
+	 * ヘッダー列数分の空の値を返す。
+	 * 行作成時に必要な空の列群。
+	 * @return {array}
+	 * @requires headerRow.length
+	 * @see #_createEstimateRow
+	 */
+	fn._createEmptyCols = function () {
+
+		return new Array(headerRow.length);
+
+	};
+
+
+	/**
+	 * 引数に指定された列名から列のindexを返す。
+	 * @param {string} col_name 列の名前
+	 * @return {number} 列のindex
+	 * @requires headerRow.indexOf
+	 * @see #_createEstimateRow
+	 * @see #_createCommodityRow
+	 */
+	fn.getColIndexOf = function (col_name) {
+
+		return headerRow.indexOf(col_name)
+
+	};
+
+
+	/**
+	 * 見積書用の最初の行の列を作成して返す。
+	 * @return {array} 列の配列。
+	 * @requires #_createEmptyCols
+	 * @requires .model.attributes
+	 * @requires .model.clientProfile.attributes
+	 * @requires #getColIndexOf
+	 * @see #_create
 	 * @todo もうちょいきれいに
 	 **/
 	fn._createEstimateRow = function () {
 
-		var cols = new Array(headerRow.length);
+		var cols = this._createEmptyCols();
 		var attrs = this.model.attributes;
 		var client = this.model.clientProfile.attributes;
 
-		cols[headerRow.indexOf('行形式')] = attrs.type;
-		cols[headerRow.indexOf('書類番号')] = attrs.id;
-		cols[headerRow.indexOf('発行日')] = attrs.issueDate;
+		cols[this.getColIndexOf('行形式')] = attrs.type;
+		cols[this.getColIndexOf('書類番号')] = attrs.id;
+		cols[this.getColIndexOf('発行日')] = attrs.issueDate;
 
-		cols[headerRow.indexOf('クライアント名')] = client.name;
-		cols[headerRow.indexOf('クライアント住所形式')] = client.address_type;
-		cols[headerRow.indexOf('クライアント国名')] = client.country_name;
-		cols[headerRow.indexOf('クライアント郵便番号')] = client.zip_code;
-		cols[headerRow.indexOf('クライアント県名')] = client.prefecture_name;
-		cols[headerRow.indexOf('クライアント市区町村')] = client.address_1;
-		cols[headerRow.indexOf('クライアント番地')] = client.address_2;
-		cols[headerRow.indexOf('クライアントマンション・ビル名')] = client.address_3;
-		cols[headerRow.indexOf('クライアント電話番号')] = client.tel_number;
-		cols[headerRow.indexOf('クライアントEメール(to)')] = client.email_address;
-		cols[headerRow.indexOf('クライアントEメール(cc)')] = client.email_cc_address;
-		cols[headerRow.indexOf('クライアント担当者部門')] = client.department_name;
-		cols[headerRow.indexOf('クライアント担当者肩書')] = client.managerial_post_name;
-		cols[headerRow.indexOf('クライアント担当者')] = client.person_in_charge_name;
+		cols[this.getColIndexOf('クライアント名')] = client.name;
+		cols[this.getColIndexOf('クライアント住所形式')] = client.address_type;
+		cols[this.getColIndexOf('クライアント国名')] = client.country_name;
+		cols[this.getColIndexOf('クライアント郵便番号')] = client.zip_code;
+		cols[this.getColIndexOf('クライアント県名')] = client.prefecture_name;
+		cols[this.getColIndexOf('クライアント市区町村')] = client.address_1;
+		cols[this.getColIndexOf('クライアント番地')] = client.address_2;
+		cols[this.getColIndexOf('クライアントマンション・ビル名')] = client.address_3;
+		cols[this.getColIndexOf('クライアント電話番号')] = client.tel_number;
+		cols[this.getColIndexOf('クライアントEメール(to)')] = client.email_address;
+		cols[this.getColIndexOf('クライアントEメール(cc)')] = client.email_cc_address;
+		cols[this.getColIndexOf('クライアント担当者部門')] = client.department_name;
+		cols[this.getColIndexOf('クライアント担当者肩書')] = client.managerial_post_name;
+		cols[this.getColIndexOf('クライアント担当者')] = client.person_in_charge_name;
 
-		cols[headerRow.indexOf('通貨')] = attrs.currency_type;
+		cols[this.getColIndexOf('通貨')] = attrs.currency_type;
 
 		return cols;
 
@@ -115,33 +189,36 @@ var Table = function (model) {
 
 
 	/**
+	 * 見積書の各項目行の列を作成して返す。
+	 * @requires #_createEmptyCols
+	 * @requires .model.attributes.id
+	 * @requires #getColIndexOf
+	 * @see #_create
 	 * @todo もうちょいきれいに
 	 **/
 	fn._createCommodityRow = function (commodity) {
 
-		var cols = new Array(headerRow.length);
+		var cols = this._createEmptyCols();
 		var attrs = commodity.attributes;
 
+		cols[this.getColIndexOf('行形式')] = '項目';
+		cols[this.getColIndexOf('書類番号')] = this.model.attributes.id;
 
-		cols[headerRow.indexOf('行形式')] = '項目';
-		cols[headerRow.indexOf('書類番号')] = this.model.attributes.id;
-
-		cols[headerRow.indexOf('項目タイプ')] = attrs.view_type;
-		//cols[headerRow.indexOf('商品コード')] = '';
-		cols[headerRow.indexOf('項目名')] = attrs.name;
-		cols[headerRow.indexOf('項目単価')] = attrs.unit_price;
-		cols[headerRow.indexOf('項目数量')] = attrs.quantity;
-		cols[headerRow.indexOf('項目単位')] = attrs.unit_name;
-		cols[headerRow.indexOf('項目金額')] = attrs.price;
+		cols[this.getColIndexOf('項目タイプ')] = attrs.view_type;
+		//cols[this.getColIndexOf('商品コード')] = '';
+		cols[this.getColIndexOf('項目名')] = attrs.name;
+		cols[this.getColIndexOf('項目単価')] = attrs.unit_price;
+		cols[this.getColIndexOf('項目数量')] = attrs.quantity;
+		cols[this.getColIndexOf('項目単位')] = attrs.unit_name;
+		cols[this.getColIndexOf('項目金額')] = attrs.price;
 		/** @todo ハードコーディングじゃなく */
-		cols[headerRow.indexOf('項目消費税対象')] = 'TRUE';
-		cols[headerRow.indexOf('項目消費税込み')] = 'FALSE';
-		cols[headerRow.indexOf('項目源泉税対象')] = 'FALSE';
+		cols[this.getColIndexOf('項目消費税対象')] = 'TRUE';
+		cols[this.getColIndexOf('項目消費税込み')] = 'FALSE';
+		cols[this.getColIndexOf('項目源泉税対象')] = 'FALSE';
 
 		return cols;
 
 	};
-
 
 })(Table.prototype);
 
